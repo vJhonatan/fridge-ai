4f32a3d68c47b7a1fbe52ef6efaf74bfd269f5d8
vJhonatan <jhonatan99653@gmail.com>
2025-07-01 01:06:31 -0300

    first commit

---
+++ src/main/java/dev/project/FridgeAi/FridgeAiApplication.java
@@ -0,0 +1,13 @@
+package dev.project.FridgeAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/project/FridgeAi/FridgeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.project.FridgeAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
